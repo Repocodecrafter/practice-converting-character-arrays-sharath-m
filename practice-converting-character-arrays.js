90df0c60e555b87a6437df370676dab0373d0d3b
Sharath Meghana <marellameghana99@gmail.com>
2025-10-18 11:28:57 -0500

    Implemented message reversal with split-reverse-join

---
+++ practice-converting-character-arrays.js
@@ -0,0 +1,36 @@
+//Task 1:
+
+let message1 = " !yako eb ll'uoy dna ,gniog peeK !sgnittes fo yteirav a ni slliks gnidoc esu osla nac uoY !ti teg ll'uoy ,tsisrep uoy fi tub ,tsrif ta drah mees yam gnidoC";
+let message2 = "!ecitcarp htiw retteb teg ll‘uoy ,emit ekaT .tsrif ta drah leef lliw gnitirw edoc tuB";
+let message3 = "!elpoep rehto morf tnereffid on era uoy ,elbuort evah uoy fI .lanoisseforp a ekil leef ot evah t'nod uoY";
+let message4 = ".rettam llits yeht ,smargorp llams etirw ylno nac uoy fI .tnemom tcefrep eht rof tiaw t'noD .yadot trats tsuJ";
+let result1 = message1.split("").reverse().join("");
+let result2 = message2.split("").reverse().join("");
+let result3 = message3.split("").reverse().join("");
+let result4 = message4.split("").reverse().join("");
+console.log("reversed message1: ", result1);
+console.log("reversed message2: ", result2);
+console.log("reversed message3: ", result3);
+console.log("reversed message4: ", result4);
+
+
+//Task 2:
+//First quote
+let msg1 = "Dream, dream, dream. Dreams transform into thoughts, and thoughts result in action.";
+let reversedMsg1 = msg1. split("").reverse().join("");
+console.log("reversedMsg1: ", reversedMsg1);
+
+//Second quote
+let msg2 = "Manpower without unity is not a strength unless it is harmonized and united properly.";
+let reversedMsg2 = msg2. split("").reverse().join("");
+console.log("reversedMsg2: ", reversedMsg2);
+
+//Third quote
+let msg3 = "Believe in yourself and all that you are.";
+let reversedMsg3 = msg3.split("").reverse().join("");
+console.log("reversedMsg3: ", reversedMsg3);
+
+//Fourth quote
+let msg4 = "Keep moving forward with courage.";
+let reversedMsg4 = msg4.split("").reverse().join("");
+console.log("reversedMsg4: ", reversedMsg4);
